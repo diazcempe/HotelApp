7764eaddeb2be68e740758b7d71704d1439442f8
Nastio Diaz <nastio.diaz@lyteventures.com>
2019-07-29 14:27:50 +0800

    Service Config: Setup SimpleInjector

--- HotelApp.Backend/HotelApp.Api/Startup.cs
+++ HotelApp.Backend/HotelApp.Api/Startup.cs
@@ -10,11 +10,21 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using SimpleInjector.Integration.AspNetCore.Mvc;
 
 namespace HotelApp.Api
 {
+    /// <summary>
+    /// References:
+    /// Simple Injector
+    /// https://simpleinjector.readthedocs.io/en/latest/aspnetintegration.html
+    /// </summary>
     public class Startup
     {
+        private Container _container = new Container();
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -22,15 +32,51 @@ namespace HotelApp.Api
 
         public IConfiguration Configuration { get; }
 
+        #region SimpleInjector
+        private void InitializeContainer()
+        {
+
+        }
+        #endregion
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            #region SimpleInjector
+            services.AddSimpleInjector(_container, options =>
+            {
+                // AddAspNetCore() wraps web requests in a Simple Injector scope.
+                options.AddAspNetCore()
+                    // Ensure activation of a specific framework type to be created by
+                    // Simple Injector instead of the built-in configuration system.
+                    .AddControllerActivation()
+                    .AddViewComponentActivation()
+                    .AddPageModelActivation()
+                    .AddTagHelperActivation();
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            #region SimpleInjector
+            // UseSimpleInjector() enables framework services to be injected into
+            // application components, resolved by Simple Injector.
+            app.UseSimpleInjector(_container, options =>
+            {
+                // Add custom Simple Injector-created middleware to the ASP.NET pipeline.
+
+                // Optionally, allow application components to depend on the
+                // non-generic Microsoft.Extensions.Logging.ILogger abstraction.
+                options.UseLogging();
+            });
+
+            InitializeContainer();
+            #endregion
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -41,8 +87,13 @@ namespace HotelApp.Api
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            #region SimpleInjector
+            _container.Verify();
+            #endregion
+
             app.UseMvc();
         }
+
+
     }
 }
