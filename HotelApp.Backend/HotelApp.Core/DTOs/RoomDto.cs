08346ef2065a928177a77d4cdd3e417c88f9a3f1
Nastio Diaz <nastio.diaz@lyteventures.com>
2019-07-30 01:13:29 +0800

    EF: Created DTOs

---
+++ HotelApp.Backend/HotelApp.Core/DTOs/RoomDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HotelApp.Core.Enums;
+
+namespace HotelApp.Core.DTOs
+{
+    public class RoomDto
+    {
+        public int Id { get; set; }
+
+        public int Number { get; set; }
+
+        public string Name { get; set; }
+
+        public RoomStatus Status { get; set; }
+
+        public bool AllowedSmoking { get; set; }
+    }
+}
