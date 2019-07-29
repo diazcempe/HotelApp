8a4e9c0018ce94ed9574716bc4a31866d7c01e7a
Nastio Diaz <nastio.diaz@lyteventures.com>
2019-07-29 12:56:34 +0800

    EF: Created Entity Models and Enums

---
+++ HotelApp.Backend/HotelApp.Core/Enums/Enums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelApp.Core.Enums
+{
+    public enum RoomStatus
+    {
+        Unavailable = 0,
+        Available = 1,
+    }
+}
