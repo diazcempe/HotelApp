f7c2b0e025dc0509cbbc70bcd1d96c2ab7126572
Nastio Diaz <nastio.diaz@lyteventures.com>
2019-08-05 08:24:10 +0800

    MVC: Added Reservations Controller

---
+++ HotelApp.Backend/HotelApp.Api/Controllers/ReservationsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelApp.Core.Data;
+using HotelApp.Core.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelApp.Api.Controllers
+{
+    [ApiController]
+    public class ReservationsController : Controller
+    {
+        private readonly IReservationRepository _reservationRepository;
+
+        public ReservationsController(IReservationRepository reservationRepository)
+        {
+            _reservationRepository = reservationRepository;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<List<ReservationDto>> List()
+        {
+            return await _reservationRepository.GetAll<ReservationDto>();
+        }
+    }
+}
